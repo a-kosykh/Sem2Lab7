f036fc661a870b423f75f69cb24e53cc853b6f37
Alex <kik9399@mail.ru>
2017-05-18 23:16:19 +0300

    fix #4

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -9,4 +9,15 @@ SCENARIO ("constructorTest"){
 	cls = mat.getColumns();
 	REQUIRE (rows == cls);
 }
-
+SCENARIO("addTest"){
+	Matrix<int> mat3(3,3);
+	std::ifstream("mat3.txt");
+	
+	Matrix<int> mat4(3,3);
+	std::ifstream ("mat4.txt") >> mat4;
+	
+	Matrix<int> matRes(3,3);
+	std::ifstream("addRes.txt") >> matRes;
+	
+	REQUIRE ((mat3 + mat4) == matRes);
+}
